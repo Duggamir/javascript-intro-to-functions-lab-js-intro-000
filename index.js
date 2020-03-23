b30cb30bace51b189a16de4665773325444cb8ff
Simon Teo <teosimon78@gmail.com>
2020-03-23 17:31:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string)
 {
   if (string === "I love you, Grandma.")
   {
-    return "I love you too."
+    return "I love you, too."
   }
   else if (string === string.toUpperCase())
   {
